3609f1a70674a832820553c92fed115fc454b0d0
isachan <isachanjh@gmail.com>
2021-03-05 23:39:59 +0800

    01-setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// use describe, it pattern
+describe('<App />', () => {
+  it('Renders <App /> component correctly', () => {
+    const { getByText } = render(<App />);
+    expect(
+      getByText(/Getting started with React testing library/i)
+    ).toBeInTheDocument();
+  });
 });
